49bfe4f022107408fbddd783e592b1988c67778f
Artem <artem.semenov.101@gmail.com>
2023-03-05 19:10:51 +0200

    little fix

--- src/components/PageScrollIndicator/PageScrollIndicator.tsx
+++ src/components/PageScrollIndicator/PageScrollIndicator.tsx
@@ -1,36 +1,35 @@
 import "./PageScrollIndicator.scss";
 import { smoothScroll } from "utils/helpers/smoothScroll";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 type Props = {};
 const PageScrollIndicator = (props: Props) => {
-  const windowHeight : number = window.innerHeight;
+  const windowHeight: number = window.innerHeight;
   let pageHeight: number;
-  useEffect(() => {
-    const body = document.body,
-      html = document.documentElement;
 
-      pageHeight = Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    );
+  const body = document.body,
+    html = document.documentElement;
 
-  });
+  pageHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
 
   const [progress, setProgress] = useState(0);
   const [windowScroll, setWindowScroll] = useState(0);
 
   document.addEventListener("scroll", () => {
     setWindowScroll(window.scrollY);
-    setProgress((window.scrollY * 100) /  (pageHeight - windowHeight));
-    if (window.scrollY === pageHeight - windowHeight) setProgress(100)
+    setProgress((window.scrollY * 100) / (pageHeight - windowHeight));
+    if (window.scrollY === pageHeight - windowHeight) setProgress(100);
   });
 
   return (
-    <div onClick={() => smoothScroll('pageTop')}
+    <div
+      onClick={() => smoothScroll("pageTop")}
       className="page-scroll-indicator"
       style={{
         background: `conic-gradient( #0500FF ${progress}%, #6B6B6B ${progress}%)`,
