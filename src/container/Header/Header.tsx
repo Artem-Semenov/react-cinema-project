d7a41dff102eabbeb749aa1858531caf0aa95772
Artem <artem.semenov.101@gmail.com>
2023-03-05 19:12:49 +0200

    lint

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -5,35 +5,41 @@ import { classToggle } from "utils/helpers/classtoggle";
 import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
-  let location = useLocation(); 
-  console.log(location)
+  let location = useLocation();
   return (
     <div className="header">
       <Container>
-        {location.pathname === '/schedule' ? <h1 className="center-logo">
-       <NavLink to={'/'}>CINEPLEX</NavLink> 
-        </h1> : ''}
-        <div className= {location.pathname === '/schedule' ? "header__wrapper schedule__header-wrapper" : "header__wrapper"} >
+        {location.pathname === "/schedule" ? (
+          <h1 className="center-logo">
+            <NavLink to={"/"}>CINEPLEX</NavLink>
+          </h1>
+        ) : (
+          ""
+        )}
+        <div
+          className={
+            location.pathname === "/schedule"
+              ? "header__wrapper schedule__header-wrapper"
+              : "header__wrapper"
+          }>
           <div
             onClick={() => {
               classToggle("burger-body", "show");
               classToggle("burger-button", "show");
             }}
             className="burger-menu__button"
-            id="burger-button"
-          >
+            id="burger-button">
             <span></span>
           </div>
-          <a href="tel:0660000000" className="tel"> </a>
+          <a href="tel:0660000000" className="tel">
+            {" "}
+          </a>
           <nav>
             <ul className="header__body">
               <li>
-                <NavLink to={'/schedule'}>
-                <button >
-                  Розклад
-                </button>
+                <NavLink to={"/schedule"}>
+                  <button>Розклад</button>
                 </NavLink>
-               
               </li>
               <div className="burger-menu__body" id="burger-body">
                 <li>
